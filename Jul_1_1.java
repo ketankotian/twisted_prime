3063153883bcc9a5acb4352e0e46ab7014003d6c
ketankotian <krkkotian@gmail.com>
2024-07-03 03:16:33 +0530

    Twisted Prime code

---
+++ Jul_1_1.java
@@ -0,0 +1,38 @@
+package twisted_prime;
+
+public class Jul_1_1 {
+	
+	public static void main(String[] args) {
+		
+		// ip
+					int num = 19;
+
+					// process
+					boolean isPrime = true, isTp = true;
+					int ld, rev = 0;
+					System.out.print("number : " + num);
+					for (int i = 2; i < num; i++) {
+						if (num % i == 0)
+							isPrime = false;
+					}
+					while (num != 0) {
+						ld = num % 10;
+						rev = ld + rev * 10;
+						num = num / 10;
+					}
+					System.out.println(", reverse : " + rev);
+
+					for (int i = 2; i < rev; i++) {
+						if (rev % i == 0)
+							isTp = false;
+					}
+					if (isPrime && isTp) {
+						System.out.println("TP");
+					} else
+						System.out.println("NTP");
+
+
+		
+	}
+	
+			}
